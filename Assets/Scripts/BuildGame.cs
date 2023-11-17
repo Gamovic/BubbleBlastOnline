4aa629407055df1782a5c31b51c14a63834b467e
Frederik <rennowfred@gmail.com>
2023-11-17 15:50:27 +0100

    build script

---
+++ Assets/Scripts/BuildGame.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEditor;
+
+public class BuildGame
+{
+    [MenuItem("Build/BuildTheGame")]
+    public static void BuildTheGame()
+    {
+        // Get the first scene
+        string scenePath = EditorBuildSettings.scenes[0].path;
+
+        // Build settings
+        BuildPlayerOptions buildOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { scenePath },
+            locationPathName = "Builds/BubbleBlastOnline",
+            target = BuildTarget.StandaloneWindows,
+            options = BuildOptions.None
+        };
+
+        // Perform build
+        BuildPipeline.BuildPlayer(buildOptions);
+
+        Debug.Log("Build completed!");
+    }
+}
